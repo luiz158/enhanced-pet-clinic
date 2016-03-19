2cd9e0ad5a9094712233f3d3ab31f06f0abfa9a9
takik <tarak.akik@gmail.com>
2016-03-19 20:19:56 +0100

    Dockerfile + entery point

---
+++ pet-clinic-startup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# ATM run server
+cd /tmp/enhanced-pet-clinic
+
+echo " -- launching enhanced-pet-clinic : java -jar enhanced-pet-clinic-exec.jar "
+java -jar enhanced-pet-clinic-exec.jar 
