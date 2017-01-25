41d46841849893a95895f5e23862470231bb6c92
rhyno.j <rhyno.j@kakaocorp.com>
2017-01-25 19:57:56 +0900

    remove

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ID=yoonsung
-PW=wlsdud891
-EMAIL=lvev9925@naver.com
+ID=
+PW=
+EMAIL=
 TAG=1.0
 
 gradle deployDockerImage -PDOCKER_USERNAME=$ID -PDOCKER_PASSWORD=$PW -PDOCKER_EMAIL=$EMAIL -PDOCKER_TAG=$TAG -x test
